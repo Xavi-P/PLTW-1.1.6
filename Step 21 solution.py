8a244a83e06fe8e7c1e3d54af047397da8c2f046
Xavi-P <Xman08711@gmail.com>
2026-02-01 12:11:31 -0500

    Implement turtle drawing with branches
    Added turtle graphics code to draw branches.

--- Step 21 solution.py
+++ Step 21 solution.py
@@ -1,1 +1,24 @@
+import turtle as trtl
 
+painter = trtl.Turtle()
+painter.pensize(40)
+
+painter.circle(5)
+
+num_branches =5
+# y = 70
+branch_length =50
+branch_angle_range = 180 / num_branches
+print("z=", branch_angle_range)
+painter.pensize(5)
+branch_num= 0
+while (branch_num < num_branches):
+  print("z*n=", branch_angle_range*branch_num)
+  painter.goto(0,0)
+  painter.setheading(branch_angle_range*branch_num)
+  painter.forward(branch_length)
+  # n=next step's angle
+  branch_num = branch_num + 1
+painter.hideturtle()
+wn = trtl.Screen()
+wn.mainloop()
